316497b025b3b490a59e256c7e12205eb4d6ba51
qinzhounick <112423678+qinzhounick@users.noreply.github.com>
2022-11-03 09:57:34 -0500

    Update Case_Study_2.m

--- Case_Study_2.m
+++ Case_Study_2.m
@@ -17,7 +17,9 @@ A = [0.95,0.05,0,0;
      0.05,0.85,0,0;
      0,0.10,1,0;
      0,0.01,0,1];
-
+X = [0.5 0.01 0.1 0.4 0.9 0.1 0 0];
+y = siroutput_full(X,1000);
+plot(y);
 
 
 % function timeMatrix = model(t)
@@ -27,20 +29,20 @@ A = [0.95,0.05,0,0;
 
 
 
-matrix = zeros(4,100);
-matrix(:,1)=A * transpose([1,0,0,0]);
+%matrix = zeros(4,100);
+%matrix(:,1)=A * transpose([1,0,0,0]);
 
-for i = 2:100
-    matrix(:,i)=A*matrix(:,i-1);
-end
+%for i = 2:100
+%    matrix(:,i)=A*matrix(:,i-1);
+%end
 
-figure(1)
-plot(1:100, matrix(1,:));
-hold on;
-plot(1:100, matrix(2,:));
-plot(1:100, matrix(3,:));
-plot(1:100, matrix(4,:));
-legend('Susceptible', 'Infected', 'Recovered', 'Dead')
+%figure(1)
+%plot(1:100, matrix(1,:));
+%hold on;
+%plot(1:100, matrix(2,:));
+%plot(1:100, matrix(3,:));
+%plot(1:100, matrix(4,:));
+%legend('Susceptible', 'Infected', 'Recovered', 'Dead')
 
 % The model assumes the following happens over each day.
 % • 5% of the susceptible population will acquire the disease. (The other 95%
